4f5e02cc2c8918a46da99409b06b997d3e11a753
jchodyla <jakub.chodyla@gmail.com>
2023-12-03 15:27:41 +0100

    Added chessboard pattern on home screen and second screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:carcreator/pages/carTypeSelection.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +9,38 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          // Chessboard pattern
+          Positioned(
+            bottom: 0,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: 100,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Column(
+                children: List.generate(
+                  4,
+                      (i) => Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: List.generate(
+                      15,
+                          (j) => Container(
+                        width: MediaQuery.of(context).size.width / 15,
+                        height: 25,
+                        decoration: BoxDecoration(
+                          color: (i % 2 == 0)
+                              ? (j % 2 == 0) ? Colors.black : Colors.white
+                              : (j % 2 == 0) ? Colors.white : Colors.black,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          // Other widgets
           Positioned(
             top: -120,
             left: 20,
@@ -58,13 +91,18 @@ class HomePage extends StatelessWidget {
                     width: 0.9 * MediaQuery.of(context).size.width,
                     child: ElevatedButton(
                       onPressed: () {
-                        // Handle button press
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CarTypeSelection(),
+                          ),
+                        );
                       },
                       style: ElevatedButton.styleFrom(
                         padding: const EdgeInsets.all(20.0),
                         backgroundColor: Colors.red,
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+                          borderRadius: BorderRadius.circular(40.0),
                         ),
                       ),
                       child: const Text(
@@ -86,7 +124,7 @@ class HomePage extends StatelessWidget {
                         padding: const EdgeInsets.all(20.0),
                         backgroundColor: Colors.red,
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
+                          borderRadius: BorderRadius.circular(40.0),
                         ),
                       ),
                       child: const Text(
