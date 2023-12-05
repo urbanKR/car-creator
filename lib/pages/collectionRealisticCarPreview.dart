ab9a5f353ab9c1f723cb75cc999fd55f4d6536f7
Krzysztof Urban <urbankrzysz36@gmail.com>
2023-12-05 20:16:35 +0100

    Added collectionRealisticCarPreviewPage, CarPreview Routing to this page, sample image

--- lib/pages/collectionRealisticCarPreview.dart
+++ lib/pages/collectionRealisticCarPreview.dart
@@ -1,17 +1,18 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
-import 'package:carcreator/pages/collectionRealisticCarPreview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class CollectionCarPreview extends StatefulWidget {
-  const CollectionCarPreview({Key? key}) : super(key: key);
+class CollectionRealisticCarPreview extends StatefulWidget {
+  const CollectionRealisticCarPreview({Key? key}) : super(key: key);
 
   @override
-  _CollectionCarPreviewState createState() => _CollectionCarPreviewState();
+  _CollectionRealisticCarPreviewState createState() =>
+      _CollectionRealisticCarPreviewState();
 }
 
-class _CollectionCarPreviewState extends State<CollectionCarPreview> {
+class _CollectionRealisticCarPreviewState
+    extends State<CollectionRealisticCarPreview> {
   final List<String> carArray = const [
     //wrong size first??
     // 'assets/graphics/Car1.svg',
@@ -101,7 +102,7 @@ class _CollectionCarPreviewState extends State<CollectionCarPreview> {
                       translation: const Offset(0.0, 0.01),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-                        //Car Preview Items
+                        //Realistic Car Preview Items
                         children: [
                           const SizedBox(
                             height: 10,
@@ -113,50 +114,38 @@ class _CollectionCarPreviewState extends State<CollectionCarPreview> {
                               fontWeight: FontWeight.bold,
                             ),
                           ),
+                          const Text(
+                            'Concept',
+                            style: TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
                           const SizedBox(
                             height: 50,
                           ),
-                          //Car svg
+                          //Realistic Car image
                           SizedBox(
-                            width: 260,
-                            height: 260,
+                            width: 330,
+                            height: 330,
                             child: Container(
                               padding: const EdgeInsets.all(8.0),
-                              child: SvgPicture.asset(carArray[0]),
+                              child: Container(
+                                padding: const EdgeInsets.all(8.0),
+                                decoration: BoxDecoration(
+                                  color: Colors.transparent,
+                                  borderRadius: BorderRadius.circular(25.0),
+                                  image: DecorationImage(
+                                    image: AssetImage(
+                                        'assets/images/realistic_car_concept_sample.png'),
+                                    fit: BoxFit.cover,
+                                  ),
+                                ),
+                              ),
                             ),
                           ),
                           const SizedBox(
                             height: 20,
-                          ),
-                          //btn
-                          SizedBox(
-                            width: 0.9 * MediaQuery.of(context).size.width,
-                            child: ElevatedButton(
-                              onPressed: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) =>
-                                        const CollectionRealisticCarPreview(),
-                                  ),
-                                );
-                              },
-                              style: ElevatedButton.styleFrom(
-                                padding: const EdgeInsets.all(20.0),
-                                backgroundColor: Colors.red,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(40.0),
-                                ),
-                              ),
-                              child: const Text(
-                                'Show concept',
-                                style: TextStyle(
-                                  fontSize: 30,
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                            ),
                           ),
                           const SizedBox(
                             height: 10,
