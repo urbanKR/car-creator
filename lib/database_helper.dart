ee39a13b52804fe64e70fd06dbb2acc54d59bfa2
Franz Lee <ff.lepka@gmail.com>
2024-01-30 18:55:45 +0100

    modified:   lib/database_helper.dart 	new file:   lib/unit_tests/unit_tests.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -72,8 +72,7 @@ CREATE TABLE $tableCars (
     final result = await db.query(tableCars);
 
     List<Car> cars = result.map((json) => Car.fromJson(json)).toList();
-    for(Car car in cars) {
-    }
+    for (Car car in cars) {}
 
     return result.map((json) => Car.fromJson(json)).toList();
   }
@@ -104,5 +103,4 @@ CREATE TABLE $tableCars (
 
     db.close();
   }
-
 }
